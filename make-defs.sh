3e6167148cecbb395f4848f3c1a74db629fd3421
Jiri (George) Lebl <jiri.lebl@gmail.com>
2019-03-15 21:59:30 -0500

    maybe it'll work now

--- make-defs.sh
+++ make-defs.sh
@@ -4,6 +4,7 @@ for n in sec_* ; do
 	echo "openDate = 01/01/19, 0600" > "set$n.def"
 	echo "dueDate = 01/01/29, 2359" >> "set$n.def"
 	echo "answerDate = 01/01/29, 2359" >> "set$n.def"
+	echo "problemList =" >> "set$n.def"
 	for m in `ls $n/*.pg` ; do
 		echo "diffyqs-webwork/$m,	1" >> "set$n.def"
 	done
