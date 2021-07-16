b6c94d8c8268937b11c07003b64292f9b4e768f2
Jiri (George) Lebl <jiri.lebl@gmail.com>
2021-07-16 16:11:15 -0700

    Tags, modify stats scripts to keep track of how much work there is still to do

--- stats.sh
+++ stats.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 for n in sec_* ; do
-	printf '%-10s %2d exercises (%2d with solution)\n' "$n" `ls -1 $n/*.pg | wc -l` `grep -l '^BEGIN_.*SOLUTION' $n/*.pg | wc -l`
+	printf '%-10s %2d exercises (%2d with solution, %2d untagged)\n' "$n" `ls -1 $n/*.pg | wc -l` `grep -l '^BEGIN_.*SOLUTION' $n/*.pg | wc -l` `grep -L '^## TitleText1.*Notes on Diffy Qs' $n/*.pg | wc -l`
 done
 
 echo
 echo Total: `ls -1 */*.pg | wc -l` exercises
 echo With solutions: `grep -l '^BEGIN_.*SOLUTION' */*.pg | wc -l` exercises
+echo Still untagged: `grep -L '^## TitleText1.*Notes on Diffy Qs' */*.pg | wc -l` exercises
