7d9aee1cd53756c3e222796a02f6c0a7a6aad541
Jiri (George) Lebl <jiri.lebl@gmail.com>
2024-09-01 20:10:20 -0500

    Some notes to avoid being confused

--- make-tar.sh
+++ make-tar.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+echo Restoring mtime...
 git restore-mtime
+echo Deleting old tgz ...
 rm -i diffyqs-webwork.tgz
 cd ..
+echo Making new tgz ...
 GZIP=-9 tar cvzf diffyqs-webwork.tgz --sort=name --exclude=".*" diffyqs-webwork/
 mv diffyqs-webwork.tgz diffyqs-webwork/
